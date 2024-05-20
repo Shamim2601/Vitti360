65d979d6b91c95b4e34f86d28586becdf9ea5516
Shamim2601 <shamim2601@gmail.com>
2024-05-20 11:48:11 +0600

    reg tutor done

---
+++ server/models/regTutor.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const regTutorSchema = new Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    id:{
+        type: String,
+        required: true
+    },
+    tag:{
+        type: String,
+        required: true
+    },
+    institution: {
+        type: String,
+        required: true
+    },
+    dept: {
+        type: String,
+        required: true
+    },
+    hsc: {
+        type: String,
+        required: true
+    },
+    background: {
+        type: String,
+        required: true
+    },
+    college: {
+        type: String,
+        required: true
+    },
+    expertise: {
+        type: String,
+        required: true
+    },
+    mode: {
+        type: String,
+        required: true
+    },
+    pref: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    fb: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true,
+    },
+})
+
+
+module.exports = mongoose.model('regTutor', regTutorSchema);
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Tutor = require('../models/Tutor');
+const regTutor = require('../models/regTutor');
 
 // Routes
 
@@ -61,5 +62,72 @@ router.get('/filter-tutors', async (req, res) => {
   }
 });
 
+
+/**
+ * GET /
+ * REG Tutors
+*/
+router.get('/reg-tutor', async (req, res) => {
+  try {
+    const locals = {
+      title: "QTutor | Welcome",
+      description: "Simple Site created with NodeJs, Express & MongoDb."
+    }
+
+    res.render('regtutor', { 
+      locals,
+    });
+
+  } catch (error) {
+    console.log(error);
+  }
+
+});
+
+
+/**
+ * POST /
+ * reg tutor
+*/
+router.post('/reg-tutor', async (req, res) => {
+  try {
+    const tutors = await Tutor.find({}, { id: 1 }).sort({ id: -1 }).limit(1); // Find the tutor with the highest ID
+    let highestId = 1;
+
+    if (tutors.length > 0) {
+        const highestIdStr = tutors[0].id; // Get the highest ID from the result
+        const numericPart = parseInt(highestIdStr.substring(1)); // Extract numeric part after the prefix "t"
+        highestId = numericPart + 1; // Increment the numeric part
+    }
+
+    const newId = "t" + highestId; // Form the new ID by concatenating the prefix "t" with the incremented numeric part
+    try {
+      const newregTutor = new regTutor({
+        name: req.body.name,
+        id: newId,
+        tag: req.body.tag,
+        institution: req.body.institution,
+        dept: req.body.dept,
+        hsc: req.body.hsc,
+        background: req.body.background,
+        college: req.body.college,
+        expertise: req.body.expertise,
+        mode: req.body.mode,
+        pref: req.body.pref,
+        phone: req.body.phone,
+        fb: req.body.fb,
+        rating: 5,
+      });
+
+      await regTutor.create(newregTutor);
+      res.redirect('/');
+    } catch (error) {
+      console.log(error);
+    }
+
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = router;
